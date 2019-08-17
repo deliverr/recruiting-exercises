b40c3a3c2242640465f0047d0691c254eec20111
carlzoo <carl.j.zhou@protonmail.com>
2019-08-17 17:06:22 -0400

    add comments

--- inventory-allocator/src/main.py
+++ inventory-allocator/src/main.py
@@ -1,34 +1,23 @@
-from inventory_allocator import convert_to_json_string, find_combo
-from Models import warehouse
+"""
+Author: Carl Zhou
+Inventory Allocator Recruiting Exercise for Deliverr
+Process input of order and outputs shipment
+"""
+# encoding: utf-8
+from inventory_allocator import process_input
 import sys
 import json
 
-'''
-Author: Carl Zhou
-Process input of order and outputs shipment
-'''
-
 
 def main():
-    input = sys.stdin.readline().strip()
-    order_json_string, warehouse_json_string = convert_to_json_string(input)
-    order_list = json.loads(order_json_string)
-    warehouse_json = json.loads(warehouse_json_string)
-
-    # convert warehouse_json to list of WareHouse objects
-    warehouses = [warehouse.Warehouse(x) for x in warehouse_json]
-
-    shipments = find_combo(order_list, warehouses)
-
-    # print output
-    print('[', end='')
-    shipments_num = len(shipments)
-    for i in range(0, shipments_num):
-        print(shipments[i], end='')
-        if i < shipments_num-1:
-            print(', ', end='')
-    print(']')
-    sys.exit(0)
+    input_string = sys.stdin.readline().strip()  # read input string from STD IN
+    try:
+        result = process_input(input_string)  # process input string
+        print(result)  # print results to STDOUT
+    except json.decoder.JSONDecodeError:  # failure in parsing json
+        print("Error: Invalid Json: " + input_string)
+        sys.exit(-1)  # Error exit code
+    sys.exit(0)  # Success exit code
 
 
 if __name__ == '__main__':
