fc547208c0070a7fffa7bee708a517e4943ba2bb
Tony <tvierra25@gmail.com>
2020-08-26 12:51:48 -0700

    Adding comments.

--- inventory-allocator/src/InventoryAllocator.java
+++ inventory-allocator/src/InventoryAllocator.java
@@ -7,6 +7,9 @@ import java.util.TreeMap;
 
 public class InventoryAllocator {
 
+	/*
+	 * Private attributes that will be used through the class.
+	 */
 	private TreeMap<String, LinkedList<Products>> mainMap = new TreeMap<String, LinkedList<Products>>(
 			Collections.reverseOrder());
 	private TreeMap<String, Products> outputMap = new TreeMap<String, Products>(Collections.reverseOrder());
@@ -14,6 +17,11 @@ public class InventoryAllocator {
 	private Products tempProducts = new Products();
 
 
+	/*
+	 * The checkInventory method will accept a Products object with something like "orange, 10"
+	 * and will check through the warehouses, and their respective inventories to get the cheapest
+	 * shipping strategy.
+	 */
 	public TreeMap<String, Products> checkInventory(Products prod) {
 
 		System.out.println("Test case: " + prod);
@@ -23,6 +31,11 @@ public class InventoryAllocator {
 
 		totalSum = isBigger(prod, totalSum);
 
+		/*
+		 * These will be two preliminary checks.
+		 * 1.) If requested item is greater than total stock in all warehouses, return empty map.
+		 * 2.) If requested item doesn't exist, return empty map.
+		 */
 		if (prod.getAmount() > totalSum && totalSum != 0) {
 			System.out.println("Not enough stock, returning empty map.");
 			return new TreeMap<String, Products>();
@@ -32,10 +45,19 @@ public class InventoryAllocator {
 			return new TreeMap<String, Products>();
 		}
 
+		
+		/*
+		 * Advance for loop to iterate over the warehouses.
+		 */
 		for (Map.Entry<String, LinkedList<Products>> iterable : mainMap.entrySet()) {
 
 			testProducts = iterable.getValue();
-
+			
+			/*
+			 * Checking to see if requested item is in that warehouse.
+			 * 1.) if it does exist, set marker on location and break out.
+			 * 2.) if not, continue to next iteration (or next warehouse)
+			 */
 			for (int i = 0; i < testProducts.size(); i++) {
 				if (testProducts.get(i).getName().equals(prod.getName())) {
 					marker = i;
@@ -47,6 +69,13 @@ public class InventoryAllocator {
 			if (!flag1)
 				continue;
 
+			
+			/*
+			 * Checking 3 cases in this block.
+			 * 1.) If requested amount is the same.
+			 * 2.) If requested amount less.
+			 * 3.) If requested amount is greater
+			 */
 			if (prod.getAmount() == testProducts.get(marker).getAmount()) {
 				outputMap.put(iterable.getKey(),
 						new Products(testProducts.get(marker).getName(), testProducts.get(marker).getAmount()));
@@ -78,6 +107,10 @@ public class InventoryAllocator {
 		return outputMap;
 	}
 
+	/*
+	 * This will gather the total number of stock for one particular item and store it in
+	 * the integer "totalSum"
+	 */
 	public int isBigger(Products prod, int totalSum) {
 		for (Map.Entry<String, LinkedList<Products>> iterable : mainMap.entrySet()) {
 			testProducts = iterable.getValue();
@@ -91,6 +124,12 @@ public class InventoryAllocator {
 		return totalSum;
 	}
 
+	
+	/*
+	 * This will fill the Main warehouse map with default values so we can have something to work with.
+	 * For a more dynamic entry, the user could be asked to provide warehouse names, items and their amounts
+	 * at runtime in the console window.
+	 */
 	public void fillMap() {
 
 		LinkedList<Products> tempProducts = new LinkedList<Products>();
